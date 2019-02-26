ec2448b772a7e7767058bcfd56f1a9fa6cb4891b
Chris Alme <chris.alme@clar.io>
2019-02-26 12:06:22 -0600

    PNIX-1763 - Build and deployment plan for Clario Snowplow javascript
    Initial checkin for Snowplow customization (current impl in Plowio repo in bitbucket).

---
+++ src/js/clario.js
@@ -0,0 +1,69 @@
+;(function() {
+    window.snowplow("newTracker", "cf", "c.clario.us", {
+        appId: window.clarioTrackerData.app_id,
+        platform: "web",
+        discoverRootDomain: true,
+        post: true,
+        contexts: {
+            webPage: true,
+            performanceTiming: true,
+            gaCookies: true,
+            geolocation: false
+        }
+    });
+
+    window.snowplow('setUserId', window.clarioTrackerData.customer_id);
+
+    (function() {
+        var hits = [];
+        var allTheCookies = document.cookie.split(";");
+        ["_fbp", "_ga"].forEach(function(term) {
+            var found = allTheCookies.find(function(cookie) {
+                return cookie.indexOf(term + "=") >= 0;
+            });
+            if (found) {
+                var parts = found.split("=");
+                if (parts.length == 2) {
+                    hits.push({
+                        name: term,
+                        value: parts[1]
+                    });
+                }
+            }
+        });
+
+        if (hits.length) {
+            window.snowplow('trackPageView', null, [{
+                schema: "iglu:io.clar/cookies/jsonschema/2-0-0",
+                data: {
+                    cookies: hits
+                }
+            }]);
+        } else {
+            window.snowplow('trackPageView');
+        }
+    })();
+
+    if (window.clarioTrackerData.order_id) {
+        window.snowplow('addTrans',
+            window.clarioTrackerData.order_id,
+            window.clarioTrackerData.order_affiliation,
+            window.clarioTrackerData.order_total
+        );
+
+        if (window.clarioTrackerData.order_items) {
+            window.clarioTrackerData.order_items.forEach(function(item) {
+                window.snowplow('addItem',
+                    item.orderId,
+                    item.sku,
+                    item.name,
+                    '',
+                    item.price,
+                    item.quantity
+                );
+            });
+        }
+
+        window.snowplow('trackTrans');
+    }
+}());
